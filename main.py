e24c9423c92ceeb0950c0324758dbb362ad09bd5
jdncodes <181788284+jdncodes@users.noreply.github.com>
2025-05-07 10:40:48 +0300

    WIP/updating char count

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #imports the get_word_count func from stats.py in the project
 from stats import get_word_count
+from stats import get_character_count
 
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
+    character_count = get_character_count(book_text)
     print (f"{word_count} words found in the document")
+    print (character_count)
 
 def get_book_text(path_to_book_file):
     with open(path_to_book_file) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def get_word_count(book_text):
     split_text = book_text.split()
     return len(split_text)
+
+def get_character_count(book_text):
+    character_count = []
+    book_text = book_text.lower()
+    split_text = book_text.split()
+    return (split_text)
+
+
