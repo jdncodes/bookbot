4aff993049c65d89b1027d1ab359a118a58dafe2
jdncodes <181788284+jdncodes@users.noreply.github.com>
2025-05-07 15:48:50 +0300

    formatting print statements done, rem hardcoding needed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #imports the get_word_count func from stats.py in the project
 from stats import get_word_count
 from stats import get_character_count
+from stats import sort_list
 
 
 def main():
@@ -8,13 +9,32 @@ def main():
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
-    print (f"{word_count} words found in the document")
-    print (character_count)
+    sorted_list_char_counts = sort_list(character_count)
+    print_book_report(book_path, word_count, sorted_list_char_counts)
+
 
 def get_book_text(path_to_book_file):
     with open(path_to_book_file) as f:
         file_contents = f.read()
         return file_contents
     
+def print_book_report(book_path, word_count, sorted_list_char_counts):
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {book_path}...")
+    print ("----------- Word Count ----------")
+    print (f"Found {word_count} total words")
+    print ("--------- Character Count -------")
+    for char_dict in sorted_list_char_counts:
+        if char_dict["char"].isalpha():
+            print (f"{char_dict['char']}: {char_dict['num']}")
+        else:
+            continue
+    print ("============= END ===============")
+    
+    #Can use a if not format instead of the if loop above
+    #for item in chars_sorted_list:
+        #if not char_dict["char"].isalpha():
+            #continue
+        #print(f"etc. etc. etc.
 
 main()
--- stats.py
+++ stats.py
@@ -14,6 +14,19 @@ def get_character_count(book_text):
     
     return characters_dict
 
+def sort_list(char_count_dict):
+    sorted_list_of_dict = []
+    
+    for key, value in char_count_dict.items():
+        individual_dict = {"char": key, "num": value}
+        sorted_list_of_dict.append(individual_dict)
+    
+    # Sort using a lambda function instead of a separate sort_num function
+    sorted_list_of_dict.sort(reverse=True, key=lambda d: d["num"])
+
+    return sorted_list_of_dict
+
+
 
 
 
