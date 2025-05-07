4caa5558c63187111cc55890e41aea6a1f6a9fa4
jdncodes <181788284+jdncodes@users.noreply.github.com>
2025-05-07 10:30:18 +0300

    add stats.py, updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#imports the get_word_count func from stats.py in the project
+from stats import get_word_count
+
+
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
@@ -9,8 +13,5 @@ def get_book_text(path_to_book_file):
         file_contents = f.read()
         return file_contents
     
-def get_word_count(book_text):
-    split_text = book_text.split()
-    return len(split_text)
 
 main()
